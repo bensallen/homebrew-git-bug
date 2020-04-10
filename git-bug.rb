2916460adec57d975b26a7d99b92ad1eb323c43f
Benjamin S. Allen <bsallen@alcf.anl.gov>
2020-04-10 08:07:39 -0500

    Rev to 0.7.1

--- git-bug.rb
+++ git-bug.rb
@@ -1,12 +1,12 @@
 class GitBug < Formula
   desc "Distributed, offline-first bug tracker embedded in git, with bridges"
   homepage "https://github.com/MichaelMure/git-bug"
-  version "0.6.0"
+  version "0.7.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/MichaelMure/git-bug/releases/download/0.6.0/git-bug_darwin_amd64"
-    sha256 "50f6445a64cc4d1187537b9897619daf05aeab179df1ad92084e95340c28cc41"
+    url "https://github.com/MichaelMure/git-bug/releases/download/0.7.1/git-bug_darwin_amd64"
+    sha256 "cb0f46fe50d9a1cd1cb9853d142b82f9c762b7380ad38f972e4078bad30a05c7"
   elsif OS.linux?
   end
   
